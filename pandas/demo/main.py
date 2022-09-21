9ce1174e6f5468b80f1c5c9ad3881370aa3deeea
cleboy <1192136710@qq.com>
2022-09-21 20:31:52 +0800

    auto

---
+++ pandas/demo/main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as ps
+import numpy as np
+import moudle1 as m
+import turtle as tt
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(ps)
+    print(np)
+    print(m)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    m.test()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ pandas/demo/moudle1.py
@@ -0,0 +1,7 @@
+
+def test():
+    print("hello world")
+
+
+def test1():
+    print("test1")
